f468684c3b4a3111b9ac2518068c8c785c25ba85
AnnSyed <syedanum21@gmail.com>
2020-05-23 12:17:06 +0500

    Add Test Scripts

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -19,26 +19,40 @@ void main() {
 
     //Write Successful test
     //Successful
+    test('SucessFul Case', () async{
+      await driver.tap(find.byValueKey('mySignUpButton'));
+      await driver.tap(find.byValueKey('myUsername'));
+      await driver.waitFor(find.text(''));
+      await driver.enterText('Anum');
+      await driver.tap(find.byValueKey('myPhoneNumber'));
+      await driver.waitFor(find.text(''));
+      await driver.enterText('032218484848');
+      await driver.tap(find.byValueKey('mySwitch'));
+      await driver.tap(find.byValueKey('mySubmitButton'));
+      await driver.waitFor(find.text('Success'));
+      await driver.tap(find.byValueKey('myOkayButton'));
+    });
 
 
-//    test('Negative Test Case', () async{
-//      await driver.tap(find.byValueKey('mySignUpButton'));
-//      await driver.tap(find.byValueKey('mySubmitButton'));
-//      await driver.waitFor(find.text('Username required'));
-//    } );
-//
-//    test('Toggle Negative Case', () async{
-//
-//      await driver.tap(find.byValueKey('myUsername'));
-//     // await driver.waitFor(find.text(''));
-//      await driver.enterText('Anum');
-//      await driver.tap(find.byValueKey('myPhoneNumber'));
-//      //await driver.waitFor(find.text(''));
-//      await driver.enterText('0342554454');
-//      await driver.tap(find.byValueKey('mySubmitButton'));
-//      await driver.waitFor(find.text('Please agree to T&C'));
-//
-//    } );
+
+    test('Negative Test Case', () async{
+      await driver.tap(find.byValueKey('mySignUpButton'));
+      await driver.tap(find.byValueKey('mySubmitButton'));
+      await driver.waitFor(find.text('Username required'));
+    } );
+
+    test('Toggle Negative Case', () async{
+
+      await driver.tap(find.byValueKey('myUsername'));
+     // await driver.waitFor(find.text(''));
+      await driver.enterText('Anum');
+      await driver.tap(find.byValueKey('myPhoneNumber'));
+      //await driver.waitFor(find.text(''));
+      await driver.enterText('0342554454');
+      await driver.tap(find.byValueKey('mySubmitButton'));
+      await driver.waitFor(find.text('Please agree to T&C'));
+
+    } );
     // Test scripts
 
 
